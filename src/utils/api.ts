b44e170a19890104a25cfc2ff560df865f4ee477
Henna Maria <hennamaria2001@gmail.com>
2025-03-08 11:35:58 +0530

    apis for users added

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,1 +1,7 @@
 export const API_BASE_URL = import.meta.env.VITE_APP_API_URL;
+export const API_ADD_USER_URL = API_BASE_URL + import.meta.env.VITE_ADD_USER_URL;
+export const API_UPDATE_USER_URL = API_BASE_URL + import.meta.env.VITE_UPDATE_USER_URL;
+export const API_DELETE_USER_URL = API_BASE_URL + import.meta.env.VITE_DELETE_USER_URL;
+export const API_GET_ALL_USERS_URL = API_BASE_URL + import.meta.env.VITE_GET_ALL_USERS_URL;
+export const API_GET_THEATER_OWNERS_URL = API_BASE_URL + import.meta.env.VITE_GET_ALL_THEATER_OWNERS_URL;
+
